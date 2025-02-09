423c968465c43c9149f49a5175382e9ca388c777
Shivendra singh <shivendra7083@gmail.com>
2025-02-10 00:36:39 +0530

    Add files

---
+++ src/Datatype.kt
@@ -0,0 +1,24 @@
+fun main () {
+    // DataType and Operator
+
+    var a = 10   // Dont need to tell Datatype
+    var b : Int = 5     // If you do
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+    println()
+
+    println(a==b)
+    println(a>b)
+    println(a<b)
+    println()
+
+// Pre
+    println(++a)
+    println(a)
+ // Post
+    println(--a)
+    println(a)
+}
---
+++ src/Variable.kt
@@ -0,0 +1,11 @@
+fun main () {
+    // VAriables
+
+    var name = "Abhay"      // var variable is can be reassigned
+    name = " Shivendra"  // This is new value
+    println(name)
+
+    val city = "Lucknow"  // Val cannot be reassigned
+    //city = "Prayagraj"   // This new value cannot be reassigned
+    print(city)
+}
